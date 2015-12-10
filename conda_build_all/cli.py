9fcf33bd3c1dbd9e199c6c7a112b4842418f4c2f
Phil Elson <pelson.pub@gmail.com>
2015-12-10 07:33:48 +0000

    Fixed the --no-inspect-conda-bld-directory CLI argument.

--- conda_build_all/cli.py
+++ conda_build_all/cli.py
@@ -1,5 +1,8 @@
 import argparse
 import logging
+import os
+
+import conda_build.config
 
 import conda_build_all
 import conda_build_all.builder
@@ -22,8 +25,8 @@ def main():
     parser.add_argument('--inspect-directories', nargs='*',
         help=('Skip a build if the equivalent disribution is already '
               'available in the specified directory.'))
-    parser.add_argument('--no-inspect-conda-bld-directory', default=True,
-        action='store_false',
+    parser.add_argument('--no-inspect-conda-bld-directory', default=False,
+        action='store_true',
         help='Do not add the conda-build directory to the inspection list.')
 
     parser.add_argument('--artefact-directory',
@@ -34,44 +37,35 @@ def main():
 
     parser.add_argument("--matrix-conditions", nargs='*', default=[],
         help=("Extra conditions for computing the build matrix "
-              "(e.g. 'python 2.7.*'). "
-              "When set, the defaults for matrix-max-n-major-versions and "
-              "matrix-max-n-minor-versions are set to 0 (i.e. no limit on "
-              "the max n versions)."))
-    parser.add_argument("--matrix-max-n-major-versions", default=-1, type=int,
+              "(e.g. 'python 2.7.*'). Note, your build matrix may also be being "
+              "limited by --matrix-max-n-major-versions and "
+              "--matrix-max-n-minor-versions."))
+    parser.add_argument("--matrix-max-n-major-versions", default=2, type=int,
         help=("When computing the build matrix, limit to the latest n major "
               "versions (0 makes this unlimited). For example, if Python 1, "
               "2 and Python 3 are resolved by the recipe and associated "
               "matrix conditions, only the latest N major version will be "
-              "used for the build matrix. (default: 2 if no matrix "
-              "conditions)"))
-    parser.add_argument("--matrix-max-n-minor-versions", default=-1, type=int,
+              "used for the build matrix. (default: 2) "))
+    parser.add_argument("--matrix-max-n-minor-versions", default=2, type=int,
         help=("When computing the build matrix, limit to the latest n minor "
               "versions (0 makes this unlimited). Note that this does not "
               "limit the number of major versions (see also "
               "matrix-max-n-major-version). For example, if Python 2 and "
               "Python 3 are resolved by the recipe and associated matrix "
               "conditions, a total of Nx2 builds will be identified. "
-              "(default: 2 if no matrix conditions)"))
+              "(default: 2)"))
 
     args = parser.parse_args()
 
     matrix_conditions = args.matrix_conditions
     max_n_versions = (args.matrix_max_n_major_versions,
                       args.matrix_max_n_minor_versions)
-    if not matrix_conditions:
-        default_n_versions = 2
-    else:
-        # Unlimited.
-        default_n_versions = 0
-    if -1 in max_n_versions:
-        max_n_versions = tuple(n if n != -1 else default_n_versions
-                               for n in max_n_versions)
 
     inspection_directories = args.inspect_directories or []
+    print(args)
     if (not args.no_inspect_conda_bld_directory and
-            os.path.isdir(conda_build.config.bldpkgs_dir)):
-        inspection_directories.append(conda_build.config.bldpkgs_dir)
+            os.path.isdir(conda_build.config.config.bldpkgs_dir)):
+        inspection_directories.append(conda_build.config.config.bldpkgs_dir)
 
     artefact_destinations = []
     for channel in args.upload_channels:
