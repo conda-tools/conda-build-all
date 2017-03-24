c25e6d501d0701a398e40f53719c9464de84f2c2
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-03-24 13:19:35 +0100

    backward compat

--- conda_build_all/builder.py
+++ conda_build_all/builder.py
@@ -214,9 +214,11 @@ class Builder(object):
 
         """
         all_distros = []
-        from conda.models.dist import Dist
-        index = {Dist(key):index[key] for key in index.keys()}
-
+        try:
+            from conda.models.dist import Dist
+            index = {Dist(key):index[key] for key in index.keys()}
+        except ImportError:
+            pass
         for meta in recipes:
             distros = resolved_distribution.ResolvedDistribution.resolve_all(meta, index,
                                                                              self.matrix_conditions)
